df5d15046bdc5772183fd26f786f22b80837b803
Divine <160066288+d-mustafa@users.noreply.github.com>
2024-03-18 13:37:52 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+/// BUTTON ONE
+// event listener
+document.getElementById("btn1").addEventListener("click", pullYourselfTogether);
+
+// event function
+function pullYourselfTogether() {
+  alert("Pull yourself... TOGETHER!");
+}
+
+/// BUTTON TWO
+// event listener
+document.getElementById("btn2").addEventListener("click", soFarSoGood);
+
+// event function
+function soFarSoGood() {
+  alert("So far... SO GOOD!");
+}
+
+/// BUTTON THREE
+// event listener
+document.getElementById("btn3").addEventListener("click", youCanSayThatAgain);
+
+// event function
+function youCanSayThatAgain() {
+  alert("You can say... THAT AGAIN!");
+}
+
+/// BUTTON FOUR
+// event listener
+document
+  .getElementById("btn4")
+  .addEventListener("click", yourGuessIsAsGoodAsMine);
+
+// event function
+function yourGuessIsAsGoodAsMine() {
+  alert("Your guess is... AS GOOD AS MINE!");
+}
+
+/// BUTTON FIVE
+// event listener
+document.getElementById("btn5").addEventListener("click", speakOfTheDevil);
+
+// event function
+function speakOfTheDevil() {
+  alert("Speak of... THE DEVIL!");
+}
